0e82c7837877012956aa3ffb542e5170ce46a911
Vakan <vakan1@yandex.ru>
2025-05-16 13:59:14 +0300

    Done most of features

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,29 +1,28 @@
 public class EmployeeBook {
-    Employee[] employees = new Employee[10];
+    private Employee[] employees = new Employee[10];
     int idIncrement = 0;
 
     // Добавить работника
     public void addEmployee(String name, double salary, int state) {
-        if ( idIncrement >= employees.length) {
+        if (idIncrement >= employees.length) {
             System.out.println("Нельзя добавить контакт, закончилось место");
         } else {
-        Employee newEmployee = new Employee(name, salary, state, idIncrement);
-        employees[idIncrement] = newEmployee;
-        idIncrement++;}
+            Employee newEmployee = new Employee(name, salary, state, idIncrement);
+            employees[idIncrement] = newEmployee;
+            idIncrement++;
+        }
     }
 
     // Распечатка пользователей
     public void printAllEmployees() {
         for (Employee employee : employees) {
             if (employee != null) {
-                System.out.println("Имя сотрудника: " + employee.getName() +
+                System.out.println("id сотрудника: " + employee.getId() +
+                        ". Имя сотрудника: " + employee.getName() +
                         ". Зарплата: " + employee.getSalary() +
                         ". Отдел: " + employee.getState() +
                         ".");
-            } else {
-                System.out.println("конец списка");
-                break;
-            }
+            } else continue;
         }
     }
 
@@ -33,23 +32,67 @@ public class EmployeeBook {
         for (Employee employee : employees) {
             if (employee != null) {
                 sum += employee.getSalary();
-            }
+            } else continue;
         }
         System.out.println("Общая сумма выплат за месяц: " + String.format("%.02f", sum));
     }
 
-    // Рассчитать среднюю зарплату по отделу
-    public void calculateAverageSalaryPerState(int state) {
+    // Рассчитать среднюю зарплату
+    public void calculateAverageSalaryPerState() {
         double sum = 0;
         int count = 0;
         for (Employee employee : employees) {
-            if (employee != null && employee.getState() == state) {
+            if (employee != null) {
                 sum += employee.getSalary();
                 count++;
-            } else break;
+            } else continue;
         }
         double total = sum / count;
-        System.out.println("Средняя зарплата в месяц по отделу: " + state + ": " + String.format("%.02f", total));
+        System.out.println("Средняя зарплата в месяц: " + String.format("%.02f", total));
+    }
+
+    // Найти сотрудника с наименьшей зарплатой
+    public void findMinSalary() {
+        double min = 9999999;
+        Employee minSalaryEmployee = new Employee("",0,0,0);
+        for (int i=0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            if (employees[i] != null && employee.getSalary() < min) {
+                min = employee.getSalary();
+            minSalaryEmployee = employee;
+            } else continue;
+        }
+        System.out.println("Сотрудник с минимальной зарплатой:");
+        System.out.print("id сотрудника: ");
+        System.out.print(minSalaryEmployee.getId());
+        System.out.print(". Имя: ");
+        System.out.print(minSalaryEmployee.getName());
+        System.out.print(". Зарплата: ");
+        System.out.print(String.format("%.02f", minSalaryEmployee.getSalary()));
+        System.out.print(". Отдел: ");
+        System.out.print(minSalaryEmployee.getState());
+
+    // Найти сотрудника с наибольшей зарплатой
+    }public void findMaxSalary() {
+        double max = 0;
+        Employee maxSalaryEmployee = new Employee("",0,0,0);
+        for (int i=0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            if (employees[i] != null && employee.getSalary() > max) {
+                max = employee.getSalary();
+            maxSalaryEmployee = employee;
+            } else continue;
+        }
+        System.out.println("Сотрудник с минимальной зарплатой:");
+        System.out.print("id сотрудника: ");
+        System.out.print(maxSalaryEmployee.getId());
+        System.out.print(". Имя: ");
+        System.out.print(maxSalaryEmployee.getName());
+        System.out.print(". Зарплата: ");
+        System.out.print(String.format("%.02f", maxSalaryEmployee.getSalary()));
+        System.out.print(". Отдел: ");
+        System.out.print(maxSalaryEmployee.getState());
+
     }
 
     public int getIdIncrement() {
--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         EmployeeBook employeeBook = new EmployeeBook();
-        employeeBook.addEmployee("Иван", 50500.98, 1);
         employeeBook.addEmployee("Олег", 55100.77, 2);
         employeeBook.addEmployee("Инга", 52700.25, 1);
+        employeeBook.addEmployee("Иван", 50500.98, 1);
         employeeBook.addEmployee("Роман", 51200.12, 1);
         employeeBook.addEmployee("Игорь", 59800.52, 2);
+        employeeBook.addEmployee("Ольга", 59100.51, 1);
+        employeeBook.addEmployee("Антон", 57750.44, 2);
+        employeeBook.addEmployee("Ринат", 52810.12, 1);
+        employeeBook.addEmployee("Татьяна", 62100.77, 2);
+        employeeBook.addEmployee("Светлана", 52800.12, 1);
         employeeBook.printAllEmployees();
+        System.out.println();// распечатка всех сотрудников
         System.out.println("Всего записей - " + employeeBook.getIdIncrement());
-        employeeBook.calculateAverageSalary();
-        employeeBook.calculateAverageSalaryPerState(1);
-
+        System.out.println();
+        employeeBook.calculateAverageSalary();                                                                          // Расчёт суммы всех зарплат за месяц
+        System.out.println();
+        employeeBook.calculateAverageSalaryPerState();                                                                  // Расчет средней зарплаты в каком-то отделе
+        System.out.println();
+        employeeBook.findMinSalary();                                                                                   // Поиск сотрудника с минимальной зарплатой
+        System.out.println();
+        employeeBook.findMaxSalary();                                                                                   // Поиск сотрудника с максимальной зарплатой
+        System.out.println();
 
     }
 }
